e0e75b92d325661131753832de75063e6d239be2
bharathipraveen <nabharathi87@gmail.com>
2020-04-05 18:42:37 +0530

    added project

--- src/test/java/org/test/Type.java
+++ src/test/java/org/test/Type.java
@@ -1,5 +1,13 @@
 package org.test;
 
 public class Type {
+	private void empId() {
+		System.out.println("hi");
 
+	}
+public static void main(String[] args) {
+
+	Type t= new Type();
+	t.empId();
+}
 }
